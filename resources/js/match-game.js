0afb05368931a061db872b8b0546dd781f9894d1
Jason-Donmoyer <j.donmoyer523@gmail.com>
2019-02-10 06:04:36 -0500

    game functionality

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -5,6 +5,12 @@ var MatchGame = {};
   Renders a 4x4 board of cards.
 */
 
+$(document).ready(function() {
+  let $game = $('#game');
+  let values = MatchGame.generateCardValues();
+  MatchGame.renderCards(values, $game);
+});
+
 /*
   Generates and returns an array of matching card values.
  */
@@ -30,7 +36,35 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
+  let hslColors = [
+    'hsl(25,85%,65%)',
+    'hsl(55,85%,65%)',
+    'hsl(90,85%,65%)',
+    'hsl(160,85%,65%)',
+    'hsl(220,85%,65%)',
+    'hsl(265,85%,65%)',
+    'hsl(310,85%,65%)',
+    'hsl(360,85%,65%)'
+  ];
+ 
+  $game.empty();
+  $game.data('flippedCards', []);
+  for (let i = 0; i < cardValues.length; i++) {
+    let $newCard = $('<div class="card col-3"></div>'); 
+    let value = cardValues[i];
+    let color = hslColors[value - 1];
+    let data = {
+      value: value,
+      isFlipped: false,
+      color: color
+    };
+    $newCard.data(data);
+    $game.append($newCard);
+  }
 
+  $('.card').click(function() {
+    MatchGame.flipCard($(this), $('#game'));
+  });
 };
 
 /*
@@ -39,5 +73,35 @@ MatchGame.renderCards = function(cardValues, $game) {
  */
 
 MatchGame.flipCard = function($card, $game) {
+  if ($card.data('isFlipped')) {
+    return;
+  }
+
+  $card.css('background-color', $card.data('color'))
+       .text($card.data('value'))
+       .data('isFlipped', true);
+
+  let flippedCards = $game.data('flippedCards');
+  flippedCards.push($card);
+
+  if (flippedCards.length === 2) {
+    if (flippedCards[0].data('value') === flippedCards[1].data('value')) {
+      let matchColor = {
+        backgroundColor: 'rgb(153,153,153)',
+        color: 'rgb(204,204,204)' 
+      }
+      flippedCards[0].css(matchColor);
+      flippedCards[1].css(matchColor);
+    } else {
+      let card1 = flippedCards[0];
+      let card2 = flippedCards[1];
+      window.setTimeout(function() {
+        card1.css('background-color', 'rgb(32,64,86)').text('').data('isFlipped', false);
+        card2.css('background-color', 'rgb(32,64,86)').text('').data('isFlipped', false);
+      }, 350);
+    }
+    $game.data('flippedCards', []);
+  }
+
 
 };
